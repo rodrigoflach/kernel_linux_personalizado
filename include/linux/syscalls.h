3107ea86ceb1e291923f2f20b7f962539ccf6816
rodrigoflach <rodrigoflach5@gmail.com>
2022-01-31 20:43:23 -0300

    Executando o Tutorial 2.2

--- include/linux/syscalls.h
+++ include/linux/syscalls.h
@@ -1422,3 +1422,4 @@ long compat_ksys_semtimedop(int semid, struct sembuf __user *tsems,
 			    const struct old_timespec32 __user *timeout);
 
 #endif
+asmlinkage long sys_listProcessInfo(long pid, const char __user *buf, int size);
